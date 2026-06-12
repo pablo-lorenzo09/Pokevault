57a9bc5ed941589403ea9847d3895998841b52cf
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-06-12 15:58:08 -0300

    ADD: FUNÇÃO DE LOGOUT

--- app.py
+++ app.py
@@ -32,6 +32,11 @@ def pagina_inicial():
 def pagina_login():
     return render_template("login.html")
 
+@app.route("/logout")
+def logout():
+    session.pop("usuario_logado", None) 
+    return redirect("/")  
+
 @app.route("/login/post", methods=["POST"])
 def logar_usuario_post():
     email = request.form.get("email")
